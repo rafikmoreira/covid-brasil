edc8c3421fe419272a2658c4715224f12d513935
Rafik Moreira <rafikmoreiradias@gmail.com>
2020-03-23 21:50:48 -0300

    adicionada a sessão de status atualizada automaticamente com os dados do Brasil

---
+++ js/app.js
@@ -0,0 +1,31 @@
+// api https://covid19.mathdro.id/api/confirmed
+// br https://covid19.mathdro.id/api/countries/BR/confirmed
+// região 18
+
+moment.locale("pt-br")
+
+const getData = async (apiUrl) => {
+  try {
+    const loader = document.getElementById("loader")
+    const covidStatus = document.getElementById("covidStatus")
+    const lastUpdate = document.getElementById("lastUpdate")
+    const confirmed = document.getElementById("confirmed")
+    const active = document.getElementById("active")
+    const recovered = document.getElementById("recovered")
+    const deaths = document.getElementById("deaths")
+    const data = await axios(apiUrl).then(r => r.data[0])
+    console.log(data)
+    // set values
+    lastUpdate.innerHTML = moment(data.lastUpdate).format("LLL")
+    confirmed.innerHTML = data.confirmed
+    active.innerHTML = data.active
+    recovered.innerHTML = data.recovered
+    deaths.innerHTML = data.deaths
+    loader.classList.add("d-none")
+    covidStatus.classList.remove("d-none")
+  } catch (e) {
+    console.log("Ocorreu um erro ao tentar obter os dados.")
+  }
+}
+
+getData("https://covid19.mathdro.id/api/countries/BR/confirmed")
